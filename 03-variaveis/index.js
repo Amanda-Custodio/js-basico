25a65cd59efad0775cafa8348eef38012a736889
Amanda Custódio <amandaccustodio@hotmail.com>
2022-10-24 10:58:49 -0300

    [Aula 03 - aprendendo sobre variáveis] {24.10.2022 10:58}

---
+++ 03-variaveis/index.js
@@ -0,0 +1,13 @@
+{
+    var nomePessoa = 'Amanda'
+
+const emailPessoa = 'amanda@email.com'
+
+let telefonePessoa = '(XX)XXXX-XXXX'
+
+console.log(emailPessoa)
+}
+
+console.log(nomePessoa)
+
+console.log(telefonePessoa)
